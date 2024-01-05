0021e0d7d01637a94c9c099fa297b95e7c170e01
Julius <jjher936@learner.dvusd.org>
2024-01-05 09:30:17 -0700

    ACTUALCONVERTER

---
+++ ConverterYEA/ConverterYEA/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConverterYEA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for( int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}

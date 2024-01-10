a845d2de4cb277ff1c67a97bcc39de0c166c38e3
Julius <jjher936@learner.dvusd.org>
2024-01-10 08:38:02 -0700

    AddedMoreConversions

--- ConverterYEA/ConverterYEA/Program.cs
+++ ConverterYEA/ConverterYEA/Program.cs
@@ -11,16 +11,51 @@ namespace ConverterYEA
     {
         static void Main(string[] args)
         {
-            //for( int i = 0; i < 100; i++)
             {
-                //Console.WriteLine(i);
-                Console.WriteLine("Enter your number");
+                Console.WriteLine("Enter the number of liters");
+                int liters = 0;
+                int.TryParse(Console.ReadLine(), out liters);
+
+                Console.WriteLine("1) for fluid ounce\n2) for gills \n3) for pints\n4) for quarts\n5) for gallons\n6) for tablespoons\n7) for teaspoons\n8) for cups");
                 int num = 0;
                 int.TryParse(Console.ReadLine(), out num);
-                Console.WriteLine(num * 2);
+                /*if (num == 1)
+                    Console.WriteLine(meters * 3.28084 + " feet");
+                if (num == 2)
+                    Console.WriteLine(meters * 1.09361 + " inches");
+                Console.ReadLine();*/
+                switch (num)
+                {
+                    case 1:
+                        Console.WriteLine(liters * 33.814 + " fluid ounces");
+                        break;
+                    case 2:
+                        Console.WriteLine(liters * 8.454 + " gills");
+                        break;
+                    case 3:
+                        Console.WriteLine(liters * 2.113 + " pints");
+                        break;
+                    case 4:
+                        Console.WriteLine(liters * 1.057 + " quarts");
+                        break;
+                    case 5:
+                        Console.WriteLine(liters / 3.785 + " gallons");
+                        break;
+                    case 6:
+                        Console.WriteLine(liters * 67.628 + " tablespoons");
+                        break;
+                    case 7:
+                        Console.WriteLine(liters * 202.9 + " teaspoons");
+                        break;
+                    case 8:
+                        Console.WriteLine(liters * 4.227 + " cups");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Units");
+                        break;
+                }
                 Console.ReadLine();
             }
-            //Console.ReadKey();
         }
     }
 }
